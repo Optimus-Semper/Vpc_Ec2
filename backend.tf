6be88eca29e41a69e4d283c2bb7a3b8c3878b7db
Optimus <louismariae78@gmail.com>
2026-01-22 08:43:28 -0800

    Encrypted

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "wk6-revision2025-s3-terraform"
+    bucket = "wk6-rev*****"
     key = "VPC-25/terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "terraform2025-lock"
+    dynamodb_table = "ter*****"
     encrypt = true
   }
 }
